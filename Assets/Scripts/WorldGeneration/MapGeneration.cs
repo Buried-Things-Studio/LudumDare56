3ab72a847fe04b5ff512e116c699712c6d2835a1
JaneBooth <janekbooth@gmail.com>
2024-10-05 01:09:15 +0100

    generates coordinates and room types

---
+++ Assets/Scripts/WorldGeneration/MapGeneration.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MapGeneration : MonoBehaviour
+{
+    private int _pathLength = 10;
+    private int _branchingRooms = 10;
+    private Dictionary<Vector2Int, RoomType> _map = new Dictionary<Vector2Int, RoomType>();
+    [SerializeField] private GameObject _startRoomPrefab;
+    [SerializeField] private GameObject _normalRoomPrefab;
+    [SerializeField] private GameObject _bossRoomPrefab;
+    [SerializeField] private GameObject _hospitalRoomPrefab;
+    [SerializeField] private GameObject _shopRoomPrefab;
+    [SerializeField] private GameObject _treasureRoomPrefab;
+
+    
+
+    void Start()
+    {
+        GenerateMainPath();
+        DisplayMap();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateMainPath()
+    {
+        _map.Add(new Vector2Int(0, 0), RoomType.Start);
+        Vector2Int currentRoom = new Vector2Int(0, 0);
+        List<Vector2Int> path = new List<Vector2Int>(){currentRoom};
+        for(int i = 0; i < _pathLength - 1; i++)
+        {
+            Vector2Int newCoord = GetNextRoomCoordinate(path, currentRoom);
+            _map.Add(newCoord, RoomType.Normal);
+            currentRoom = newCoord;
+            path.Add(newCoord);
+        }
+        Vector2Int bossCoord = GetNextRoomCoordinate(path, currentRoom);
+        _map.Add(bossCoord, RoomType.Boss);
+
+        for(int i = 0; i < _branchingRooms; i ++)
+        {
+            List<Vector2Int> possibleNextRooms = GetPossibleBranchingRooms();
+            Vector2Int nextRoomCoords = possibleNextRooms[Random.Range(0, possibleNextRooms.Count)];
+            _map.Add(nextRoomCoords, RoomType.Normal);
+        }
+
+        List<Vector2Int> hospitalNextRooms = GetPossibleBranchingRooms();
+        Vector2Int hospitalRoomCoords = hospitalNextRooms[Random.Range(0, hospitalNextRooms.Count)];
+        _map.Add(hospitalRoomCoords, RoomType.Hospital);
+
+        List<Vector2Int> shopNextRooms = GetPossibleBranchingRooms();
+        Vector2Int shopRoomCoords = shopNextRooms[Random.Range(0, shopNextRooms.Count)];
+        _map.Add(shopRoomCoords, RoomType.Shop);
+
+        List<Vector2Int> treasureNextRooms = GetPossibleBranchingRooms();
+        Vector2Int treasureRoomCoords = treasureNextRooms[Random.Range(0, treasureNextRooms.Count)];
+        _map.Add(treasureRoomCoords, RoomType.Treasure);
+    }
+
+    private Vector2Int GetNextRoomCoordinate(List<Vector2Int> path, Vector2Int previousCoordinate)
+    {
+        List<Vector2Int> possibleCoords = GetPossibleNextRoom(previousCoordinate);
+        List<Vector2Int> usableCoords = new List<Vector2Int>();
+        foreach(Vector2Int coord in possibleCoords)
+        {
+            if(!path.Contains(coord) && CountAdjacencies(coord, path) == 1)
+            {
+                usableCoords.Add(coord);
+            }
+        }
+        return usableCoords[Random.Range(0, usableCoords.Count)];
+    }
+
+    private List<Vector2Int> GetAdjacentCoordinates(Vector2Int coordinate)
+    {
+        List<Vector2Int> adjacentCoords = new List<Vector2Int>(){
+            new Vector2Int(coordinate.x + 1, coordinate.y),
+            new Vector2Int(coordinate.x - 1, coordinate.y),
+            new Vector2Int(coordinate.x, coordinate.y + 1),
+            new Vector2Int(coordinate.x, coordinate.y - 1)
+        };
+        return adjacentCoords;
+    }
+
+    private int CountAdjacencies(Vector2Int coordinate, List<Vector2Int> path)
+    {
+        List<Vector2Int> adjacentCoords = GetAdjacentCoordinates(coordinate);
+        int count = 0;
+        foreach(Vector2Int coord in adjacentCoords)
+        {
+            if(path.Contains(coord))
+            {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    private List<Vector2Int> GetPossibleNextRoom(Vector2Int coordinate)
+    {
+        List<Vector2Int> adjacentCoords = new List<Vector2Int>(){
+            new Vector2Int(coordinate.x + 1, coordinate.y),
+            new Vector2Int(coordinate.x - 1, coordinate.y),
+            new Vector2Int(coordinate.x, coordinate.y + 1)
+        };
+        return adjacentCoords;
+        
+    }
+
+    private List<Vector2Int> GetPossibleBranchingRooms()
+    {
+        List<Vector2Int> allAdjacentRooms = new List<Vector2Int>();
+        List<Vector2Int> path = new List<Vector2Int>();
+
+        foreach(KeyValuePair<Vector2Int, RoomType> kvp in _map)
+        {
+            path.Add(kvp.Key);
+        }
+
+        foreach(KeyValuePair<Vector2Int, RoomType> kvp in _map)
+        {
+            if(kvp.Value == RoomType.Normal)
+            {
+                List<Vector2Int> adjRooms = GetAdjacentCoordinates(kvp.Key);
+                foreach(Vector2Int room in adjRooms)
+                {
+                    if(!allAdjacentRooms.Contains(room) && !path.Contains(room))
+                    {
+                        allAdjacentRooms.Add(room);
+                    }
+                }
+            }
+        }
+
+        List<Vector2Int> usableRooms = new List<Vector2Int>();
+        foreach(Vector2Int room in allAdjacentRooms)
+        {
+            if(CountAdjacencies(room, path) == 1)
+            {
+                usableRooms.Add(room);
+            }
+        }
+        return usableRooms;
+    }
+
+    private void DisplayMap()
+    {
+        foreach(KeyValuePair<Vector2Int, RoomType> kvp in _map)
+        {
+            if(kvp.Value == RoomType.Start)
+            {
+                Instantiate(_startRoomPrefab, new Vector3(kvp.Key.x, kvp.Key.y, 0f), Quaternion.identity);
+            }
+            if(kvp.Value == RoomType.Normal)
+            {
+                Instantiate(_normalRoomPrefab, new Vector3(kvp.Key.x, kvp.Key.y, 0f), Quaternion.identity);
+            }
+            if(kvp.Value == RoomType.Boss)
+            {
+                Instantiate(_bossRoomPrefab, new Vector3(kvp.Key.x, kvp.Key.y, 0f), Quaternion.identity);
+            }
+            if(kvp.Value == RoomType.Shop)
+            {
+                Instantiate(_shopRoomPrefab, new Vector3(kvp.Key.x, kvp.Key.y, 0f), Quaternion.identity);
+            }
+            if(kvp.Value == RoomType.Treasure)
+            {
+                Instantiate(_treasureRoomPrefab, new Vector3(kvp.Key.x, kvp.Key.y, 0f), Quaternion.identity);
+            }
+            if(kvp.Value == RoomType.Hospital)
+            {
+                Instantiate(_hospitalRoomPrefab, new Vector3(kvp.Key.x, kvp.Key.y, 0f), Quaternion.identity);
+            }
+        }
+        
+    }
+    
+}
