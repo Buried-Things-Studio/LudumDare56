9500b1f2ad757584628523bddbf140535a9b5233
Niru Fekri-Arnold <nirufekriarnold@gmail.com>
2024-10-07 21:48:07 +0100

    Nurse, all affinity icons in

--- Assets/Scripts/Globals/PictureHelpers.cs
+++ Assets/Scripts/Globals/PictureHelpers.cs
@@ -6,7 +6,18 @@ public static class PictureHelpers
 {
     public static Sprite GetProfilePicture(Critter critter)
     {
-        Debug.Log("pic sought");
         return Resources.Load<Sprite>("ProfilePics/" + critter.Name) as Sprite;
+    }
+
+
+    public static Sprite GetMoveAffinityPicture(Move move)
+    {
+        return Resources.Load<Sprite>("Affinities/" + move.Affinity.ToString()) as Sprite;
+    }
+
+
+    public static Sprite GetBugAffinityPicture(Critter critter)
+    {
+        return Resources.Load<Sprite>("Affinities/" + critter.Affinities[0].ToString()) as Sprite;
     }
 }
--- Assets/Scripts/UIControllers/MoveDetails.cs
+++ Assets/Scripts/UIControllers/MoveDetails.cs
@@ -11,9 +11,11 @@ public class MoveDetails : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _usesTMP;
     [SerializeField] private TextMeshProUGUI _powerTMP;
     [SerializeField] private TextMeshProUGUI _bluntSharpTMP;
+    [SerializeField] private TextMeshProUGUI _accuracyTMP;
     [SerializeField] private TextMeshProUGUI _descriptionTMP;
 
     [SerializeField] private Image _typeIcon;
+    [SerializeField] private Image _descriptionTypeIcon;
     [SerializeField] private Image _spikesImage;
     [SerializeField] private Image _backgroundFadeImage;
     private Move _move;
@@ -27,6 +29,13 @@ public class MoveDetails : MonoBehaviour
         _usesTMP.text = $"{move.CurrentUses}/{move.MaxUses}";
         _bluntSharpTMP.text = move.IsSharp ? "SHARP" : "BLUNT";
         _bluntSharpTMP.text = move.BasePower > 0 ? _bluntSharpTMP.text : "--";
+
+        _accuracyTMP.text = move.Accuracy.ToString() + "%";
+
+        _typeIcon.sprite = PictureHelpers.GetMoveAffinityPicture(move);
+        _descriptionTypeIcon.sprite = PictureHelpers.GetMoveAffinityPicture(move);
+        _descriptionTypeIcon.color = CritterAffinityData.GetAffinityColor(move.Affinity);
+
         _descriptionTMP.text = move.Description;
         _powerTMP.text = move.BasePower > 0 ? move.BasePower.ToString() : "--";
 
