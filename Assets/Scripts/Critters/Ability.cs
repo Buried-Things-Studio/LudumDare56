fae7bb91ccce0a7b24872bebe386d5acf8598cc5
Skyeward <mattyarnold@hotmail.co.uk>
2024-10-07 16:45:31 +0100

    ability class; TMs exist; helpers for heals at bug hospital

---
+++ Assets/Scripts/Critters/Ability.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ability
+{
+    public string Name;
+    public string Description;
+    public AbilityID ID;
+}
+
+
+public enum AbilityID
+{
+    None,
+}
